2f77b797aae0e25bb5143eb5497f9455ab6d98d4
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-06-16 00:22:56 +0100

    :sparkles: utilize the power of generics

--- src/app.ts
+++ src/app.ts
@@ -26,6 +26,33 @@ export function main() {
 
     list.render(doc, type.value, "end")
   })
+
+  //tackling Generics
+  const addUID = <T extends object>(obj: T) => {
+    let uid = Math.floor(Math.random() * 100)
+    return { ...obj, uid }
+  }
+
+  let docOne = addUID({ name: "yoshi", age: 40 })
+  console.log(docOne.name)
+
+  interface Resource<T> {
+    uid: number
+    resourceName: string
+    data: T
+  }
+
+  const docThree: Resource<object> = {
+    uid: 1,
+    resourceName: "person",
+    data: { name: "shaun" }
+  }
+
+  const docFour: Resource<string[]> = {
+    uid: 2,
+    resourceName: "teacher",
+    data: ["ken", "2", "taiwo"]
+  }
 }
 
 main()
